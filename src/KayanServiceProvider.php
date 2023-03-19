2b8c52c8cc04eff9534979aa25b76ffda0d06e4d
abdelrahman.m <abdelrahman.m@petroapp.com.sa>
2023-03-20 00:07:47 +0200

    Initial Commit

---
+++ src/KayanServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Kayan Service Provider
+ **/
+
+namespace Mrmarchone\Kayan;
+
+use Illuminate\Support\ServiceProvider;
+
+class KayanServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+
+    }
+}
